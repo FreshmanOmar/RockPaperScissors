d7a123de021472ff971aa19052e41c3ad3d321a2
Omar <Omar.abdi.2022@student.lhschools.org>
2019-05-17 08:00:00 -0400

    restart
    I'm just now making the third commit because i decided to scrap a lot of my original code and didn't want to make another commit until i felt like i knew what i was really doing and now i do :)

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,37 +1,55 @@
 package com.LickingHeights;
 
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        //declare
-        String RPS;//Rock Paper Scissors
-        Scanner input;
-        //initializing
+        do {
+            String RPS;//Rock Paper Scissors
+            Scanner input;
+            int total;
+
+            //the Users Choice
+            input = new Scanner(System.in);
+            System.out.println("Choose either Rock Paper or Scissors");
+
+          RPS = input.nextLine();
+          System.out.println(RPS);
+          String[] RandArray = {RPS};
+            for (int i = 0; i < RandArray.length; i++){
+                 total = RandArray[i].length();
+                 System.out.println(total);
+                 }
+                 String UChoice = UserChoice(RandArray);
+            System.out.println(UChoice);
+            
 
 
-        //the Users Choice
-        input = new Scanner(System.in);
-         System.out.println("You are Playing Rock paper Scissors against a computer!\n" +
-                 "Choose either Rock Paper or Scissors");
-        RPS = input.nextLine();
 
 
-        //The Computers Choice
-        int Compchoice = CompRPS();
-        System.out.println(Compchoice);
+                 //The Computers Choice
+            System.out.println("comp");
+            int Compchoice = CompRand();
+            System.out.println(Compchoice+",");
+            String WordChoice = Intconverter(Compchoice);
+            System.out.println(WordChoice);
+            String[] computer = {WordChoice};
+            for (int i = 0; i < computer.length; i++){
+                System.out.print(i);System.out.println(",");}
 
-        String Choice = theExactChoice(Compchoice);
-        System.out.println(Choice);
+        } while(7==7);
+        }  public static int CompRand () {
+    int rpsChooser = (int) (Math.random() * 12);
+    return rpsChooser;
+}
 
-        }
-        public static int CompRPS() {
-       int  rpsChooser = (int) (Math.random()*12);
-       return rpsChooser;
-    }
-    public static String theExactChoice(int Choice) {
-        switch (Choice){
+
+
+    public static String Intconverter(int Array) {
+        switch (Array) {
+            case 0:
             case 1:
             case 2:
             case 3:
@@ -46,8 +64,31 @@ public class Main {
             case 10:
             case 11:
             case 12:
-                return"Scissors";
-                }
+                return "Scissors";
+            default:}
+                return null;}
+
+
+    public static String UserChoice(String[] Array) {
+    int total;
+    for (int i = 0; i < Array.length; i++){
+            total = Array[i].length();
+            switch (total){
+                case 3:
+                case 4:
+                   return "Rock";
+
+                case 5:
+                case 6:
+                    return "Paper";
+
+                case 7:
+                case 8:
+                case 9:
+                   return "Scissors";
+                default:return "invaild";
+            }
+        }
 
 
     return null;}
@@ -179,68 +220,66 @@ public class Main {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
