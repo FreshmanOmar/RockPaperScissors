c6619cc8c1df0647dc9c8353839c293278f63cbd
Omar <Omar.abdi.2022@student.lhschools.org>
2019-05-21 08:55:38 -0400

    Finished
    I now feel like it is a finished product though i would liked to have been able to make a better way to restart the game

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,70 +6,71 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        do {
+
+
+
             String RPS;//Rock Paper Scissors
             Scanner input;
-            int total;
+            int total = 0;
+            int answer = 0;
+            String Query;
 
-            //the Users Choice
-            input = new Scanner(System.in);
-            System.out.println("Choose either Rock Paper or Scissors");
+                do {
 
-          RPS = input.nextLine();
-          System.out.println(RPS);
-          String[] RandArray = {RPS};
-            for (int i = 0; i < RandArray.length; i++){
-                 total = RandArray[i].length();
-                 System.out.println(total);
-                 }
-                 String UChoice = UserChoice(RandArray);
-            System.out.println(UChoice);
-            
+                //the Users Choice
+                input = new Scanner(System.in);
+                System.out.println("Choose Rock Paper Scissors:");
+                RPS = input.nextLine();
+                //System.out.println(RPS);
+                String[] RandArray = {RPS};
+                for (int i = 0; i < RandArray.length; i++) {
+                    total = RandArray[i].length();
+                    //System.out.println(total);
+                }
+                String UChoice = UserChoice(RandArray);
+                System.out.println("\nYou choose " + UChoice);
 
-
-
-
-                 //The Computers Choice
-            System.out.println("comp");
-            int Compchoice = CompRand();
-            System.out.println(Compchoice+",");
-            String WordChoice = Intconverter(Compchoice);
-            System.out.println(WordChoice);
-            String[] computer = {WordChoice};
+                //The Computers Choice
+                int Compchoice = CompRand();
+                // System.out.println(Compchoice+",");
+                String WordChoice = Intconverter(Compchoice);
+                System.out.println("\nThe computer choose " + WordChoice);
+           /* String[] computer = {WordChoice};
             for (int i = 0; i < computer.length; i++){
-                System.out.print(i);System.out.println(",");}
+                System.out.print(i);System.out.println(",");
+            }*/
 
-        } while(7==7);
+                System.out.println(Compare(Compchoice, total));
+                System.out.println("\nDo you wish to continue?");
+                Query = input.nextLine();
+                String[] restart = {Query};
+                for (int i = 0; i < restart.length; i++) {
+                    answer = restart[i].length();
+
+                }
+                } while (answer == 3);
+            String[] restart = {Query};
+            for (int i = 0; i < restart.length; i++) {
+                answer = restart[i].length();
+                }if (answer == 2 ){System.out.println("\nThanks for Playing");}
+
+
+
         }  public static int CompRand () {
-    int rpsChooser = (int) (Math.random() * 12);
+    int rpsChooser = (int) (Math.random() * 2);
     return rpsChooser;
 }
-
-
-
-    public static String Intconverter(int Array) {
+public static String Intconverter(int Array) {
         switch (Array) {
             case 0:
-            case 1:
-            case 2:
-            case 3:
-            case 4:
                 return "Rock";
-            case 5:
-            case 6:
-            case 7:
-            case 8:
+            case 1:
                 return "Paper";
-            case 9:
-            case 10:
-            case 11:
-            case 12:
+            case 2:
                 return "Scissors";
             default:}
                 return null;}
-
-
-    public static String UserChoice(String[] Array) {
+                public static String UserChoice(String[] Array) {
     int total;
     for (int i = 0; i < Array.length; i++){
             total = Array[i].length();
@@ -77,21 +78,31 @@ public class Main {
                 case 3:
                 case 4:
                    return "Rock";
-
-                case 5:
-                case 6:
+                   case 5:
+                       case 6:
                     return "Paper";
-
-                case 7:
+                    case 7:
                 case 8:
                 case 9:
                    return "Scissors";
                 default:return "invaild";
             }
         }
+        return null;}
+        public static String Compare(int Choice,int Total) {
+        if(Choice == 0 && (Total == 3 || Total == 4) ){return  "That's A Tie";
+        }else if (Choice == 1 && (Total == 5 || Total == 6) ){return "That's A Tie" ;
+        }else if (Choice == 2 && (Total == 7 || Total == 8 || Total == 9)){ return "That's A Tie";
+        }else if (Choice == 0 && (Total == 7 || Total == 8 || Total == 9)){return "\nRock Beats Scissor:\n You Lost!";
+        }else if (Choice == 0 && (Total == 5 || Total == 6)){return "\nPaper Beats Rock:\n You Won!";
+        }else if (Choice == 1 && (Total == 3 || Total == 4)){return"\nPaper Beats Rock:\n You Lost!";
+        }else if (Choice == 1 && (Total == 7 || Total == 8 || Total == 9)){return "\nScissor Beats Paper:\n You Won!";
+        }else if (Choice == 2 && (Total == 3 || Total == 4)){return "\nRock  Beats Scissor:\n You Won!";
+        }else if (Choice == 2 && (Total == 5 || Total == 6)){return "\nScissor Beats Paper:\n You Lost!";
+        }
+        return null;
+    }
 
-
-    return null;}
 
 
 
